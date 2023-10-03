d6b1943fcc1e2602592b4b35f3edff7f51dbc0cd
paradoxicalsphere <osterczyk@gmail.com>
2023-10-03 15:49:16 -0700

    Draft Q#01 to Q#03 in module TTT.A2

--- src/TTT/A2.hs
+++ src/TTT/A2.hs
@@ -3,19 +3,57 @@ module TTT.A2 where
 import Data.List (intercalate)
 import TTT.A1
 
+-- Completed October 3, 2023 to 
+
+-- NOTES
+
+{-
+To avoid the warning -Wmissing-home-modules when using :r or :l TTT.A2 use :m TTT.A2 to load or reload the TTT.A2 module.
+Alternately, use :m + TTT.A2 to load or reload the TTT.A2 module in addition to existing loaded modules. :m is
+equivalent to :module For more details, see https://stackoverflow.com/questions/71172186/cabal-repl-cant-load-module-in-library
+-}
+
 -- Q#01
 
-promptPlayer = undefined
+-- Define a function that constructs and returns a string used to prompt the current player to type a move
+constructPlayerPrompt :: String -> String
+constructPlayerPrompt currentPlayerString = concat ["PLAYER ", currentPlayerString, "'S TURN - Please type a row and column position (for example, A1): "]
+
+-- Define a function that displays the string prompting the current player to type a move
+promptPlayer :: Player -> String
+promptPlayer currentPlayer = constructPlayerPrompt (showSquare currentPlayer)
 
 -- Q#02
 
-_RANGE_ = undefined
+{-
+Define a list of integers used to test that players type row and column positions that exist on a Tic Tac Toe board
+
+NOTE: The upper limit of the range is defined using the _SIZE_ constant defined in module TTT.A1
+-}
+_RANGE_ :: [Int]
+--_RANGE_ = [0, 1, 2]
+_RANGE_ = [0..(_SIZE_ - 1)]
 
 -- Q#03
 
-isDigit = undefined
+-- The isDigit function returns True if the character passed into the function is a digit. Otherwise, the function returns False
+isDigit :: Char -> Bool
+isDigit inputDigit = inputDigit `elem` ['0' .. '9']
 
-readDigit = undefined
+-- The convertCharToString function converts a character into a string
+convertCharToString :: Char -> String
+convertCharToString inputChar = [inputChar]
+
+-- The convertStringToInt function converts a string into an integer
+convertStringToInt :: String -> Int
+convertStringToInt inputString = read inputString :: Int
+
+{-
+The readDigit function converts an input character into the corresponding integer, if the character is a digit.
+Otherwise, the function returns the integer -1
+-}
+readDigit :: Char -> Int
+readDigit inputDigit = if isDigit inputDigit then convertStringToInt (convertCharToString inputDigit) else (-1)
 
 -- Q#04
 
